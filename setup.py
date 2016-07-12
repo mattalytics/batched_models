8dd3b2da6acec14f008493714eae19dcbc63df10
Matt Y <matthewtyoung@gmail.com>
2016-07-12 19:04:29 -0400

    setup

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
 
-setup(name='BulkManager',
+setup(name='batched_models',
       version='0.1.0',
       description='Django Bulk Manager',
       author='Matt',
       author_email='matt@matt.com',
       url='https://www.python.org',
-      packages=['BulkManager'],
+      packages=['batched_models.BulkManager'],
      )
