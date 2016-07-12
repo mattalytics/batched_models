a554140454742a8e12b423c5af2bacf518ecfc5b
Matt Y <matthewtyoung@gmail.com>
2016-07-12 19:01:28 -0400

    setup

--- example_project/venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/matthewyoung/code/batched_models/example_project/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
